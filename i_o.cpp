e94a70afb97b2c6c677a0c83e52ac1e8710e895d
Duncan Miller <duncan@openshiro.com>
2024-05-17 10:09:24 -0700

    accept input

---
+++ i_o.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i;
+  cout << "Enter a number: ";
+  cin >> i;
+  cout << "The number you entered is " << i;
+  cout << "and its double is " << i+i << ".\n";
+  return 0;
+}
