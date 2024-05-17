9d711f89d29857eb3500204babb68ced734b247d
Duncan Miller <duncan@openshiro.com>
2024-05-17 10:12:02 -0700

    define a variable for newline

--- i_o.cpp
+++ i_o.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
+const string newline = ".\n";
 
 int main() {
   int i;
   cout << "Enter a number: ";
   cin >> i;
   cout << "The number you entered is " << i;
-  cout << "and its double is " << i+i << ".\n";
+  cout << "and its double is " << i+i << newline;
   return 0;
 }
