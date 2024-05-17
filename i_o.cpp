e622b990d6d2bbddfd830b0338d4f41c4e45e2b4
Duncan Miller <duncan@openshiro.com>
2024-05-17 10:17:13 -0700

    string input

--- i_o.cpp
+++ i_o.cpp
@@ -8,6 +8,10 @@ int main() {
   cout << "Enter a number: ";
   cin >> i;
   cout << "The number you entered is " << i;
-  cout << "and its double is " << i+i << newline;
+  cout << " and its double is " << i+i << newline;
+  string name;
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "Hi " << name << " did you like how I added " << i << " and " << i << " to get " << i+i << "?" << newline;
   return 0;
 }
